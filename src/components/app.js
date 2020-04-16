208ff488a574f390e491832e4b2ecd45b06ef790
GangGang93 <taylorlouisecoleman@gmail.com>
2020-04-16 12:49:13 -0400

    server commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router, 
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import NavigationContainter from "./navigation/navigation-container";
+import HomePage from "./pages/homepage";
+import Missions from "./pages/missions";
+import Galleries from "./pages/galleries";
+import NasaTV from "./pages/nasa_tv";
+import follow_nasa from './pages/follow_nasa';
+import downloads from './pages/downloads';
+import about from './pages/about';
+import nasa_audiences from './pages/nasa_audiences';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className='container'>
+        <Router>
+          <div>
+            <NavigationContainter />
+            <h1>NASA</h1>
+            <h2>National Aeronautics and Space Administration</h2>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/missions" component={Missions} />
+              <Route path="/galleries" component={Galleries} />
+              <Route path="/nasa_tv" component={NasaTV} />
+              <Route path="/follow_nasa" component={follow_nasa} />
+              <Route path="/downloads" component={downloads} />
+              <Route path="/about" component={about} />
+              <Route path="/nasa_audience" component={nasa_audiences} />
+            </Switch>
+          </div>
+        </Router>
+        
       </div>
     );
   }
