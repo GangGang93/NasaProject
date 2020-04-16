dd294c4fe7c5077365eed3e72941d6da4dab8871
Koy Herrick <herrickoy@gmail.com>
2020-04-16 12:22:30 -0600

    nav-container display
    creating elements in nav container so they would render on the screen

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class extends Component {
   constructor(props) {
@@ -11,9 +12,42 @@ export default class extends Component {
 
   render() {
       return (
-          <div>
-              
-          </div>
+        <div className="nav-wrapper">
+        <div className="left-side">
+            <div className="nav-link-wrapper">
+                <NavLink exact to="/">Home</NavLink>
+            </div> 
+
+            <div className="nav-link-wrapper">
+                <NavLink to="/missions">About</NavLink>
+            </div>
+
+            <div className="nav-link-wrapper">
+                <NavLink to="/galleries">Galleries</NavLink>
+            </div>   
+
+            <div className="nav-link-wrapper">
+                <NavLink to="/nasa_tv">Nasa TV</NavLink>
+            </div>        
+
+            <div className="nav-link-wrapper">
+                <NavLink to="follow_nasa">Follow NASA</NavLink>
+            </div>     
+
+            <div className="nav-link-wrapper">
+                <NavLink to="/downloads">Downloads</NavLink>
+            </div>     
+
+            <div className="nav-link-wrapper">
+                <NavLink to="/about">About</NavLink>
+            </div>     
+
+            <div className="nav-link-wrapper">
+                <NavLink to="/nasa_audience">Nasa Audience</NavLink>
+            </div>     
+        </div>     
+
+    </div>
       )
   }
 }
