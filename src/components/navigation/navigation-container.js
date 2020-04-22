2cf9f56d405b944d63e87eda55943c42b4864a0e
Koy Herrick <herrickoy@gmail.com>
2020-04-22 11:43:24 -0600

    added new branch to repo

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,69 +1,78 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Logo from '../../images/nasalogo.png';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import Logo from '../../images/nasalogo.png'
-
 library.add(faSearch);
 
-export default class NavigationContainter extends Component {
-  constructor(props) {
-      super(props)
-
-      this.state = {
-          
-      }
-    }
-
-  render() {
-      return (
-        <div className="nav-wrapper">
-            <div className="nav-link-wrapper">
-                <NavLink exact to="/" className="logo">
-                    <img src={Logo} />
-                </NavLink>
-            </div> 
-
-            <div className="center">
+export default class NavigationContainer extends Component {
+    constructor(props) {
+      super(props);
+    } 
+    
+    render() {
+        return (
+            <div className="nav-wrapper">
                 <div className="nav-link-wrapper">
-                    <NavLink to="/missions">About</NavLink>
-                </div>
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/galleries">Galleries</NavLink>
-                </div>   
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/nasa_tv">Nasa TV</NavLink>
-                </div>        
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="follow_nasa">Follow NASA</NavLink>
-                </div>     
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/downloads">Downloads</NavLink>
-                </div>     
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/about">About</NavLink>
-                </div>     
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/nasa_audience">Nasa Audience</NavLink>
+                    <NavLink exact to="/" className="logo" activeClassName="nav-link-active">
+                        <img src={Logo} />
+                    </NavLink>
                 </div> 
 
-                <div className="search-bar">
-                        <a>
+                <div className="center">
+                    <div className="nav-link-wrapper" >
+                    <a href="https://www.nasa.gov/missions" activeClassName="nav-link-active">
+                            <h1>Missions</h1>
+                        </a>
+                    </div>
+
+                    <div className="nav-link-wrapper" >
+                        <NavLink to="/galleries" activeClassName="nav-link-active">
+                            Galleries
+                        </NavLink>
+                    </div>   
+
+                    <div className="nav-link-wrapper">
+                        <a href="https://www.nasa.gov/multimedia/nasatv/index.html#public" activeClassName="nav-link-active">
+                            <h1>Nasa Tv</h1>
+                        </a>
+                    </div>        
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/follow_nasa" activeClassName="nav-link-active">
+                            Follow NASA
+                        </NavLink>
+                    </div>     
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/downloads" activeClassName="nav-link-active">
+                            Downloads
+                        </NavLink>
+                    </div>     
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about" activeClassName="nav-link-active">
+                            About
+                        </NavLink>
+                    </div>     
+
+                    <div className="nav-link-wrapper" >
+                        <NavLink to="/nasa_audience" activeClassName="nav-link-active">
+                            Nasa Audience
+                        </NavLink>
+                    </div>  
+
+                    <div className="search-bar">
+                        <a  activeClassName="nav-link-active">
                             <input id="filter" type="text" placeholder="Search" />
                             <FontAwesomeIcon icon="search" />
                         </a>
-                    </div>    
-        </div>     
-
-    </div>
-      )
-  }
-}
+                    </div>   
+                </div>     
+            </div>
+        );
+    }
+};
--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -2,42 +2,33 @@ import React, { Component } from 'react';
 import Typist from 'react-typist';
 
 
-
 export default class Homepage extends Component {
   constructor(props) {
       super(props)
 
       this.state = {
-
+          
       }
-
-      
   }
-  
-  
 
   render() {
-  
-
       return (
-          
-          <div className="background">
+        <div className="background">
           <div className="homepage-container">
-          
-            
+
             <div className="top-home">
-                <div className="top-left-home tallBox">
+                <div className="top-left-home">
                     <img src={require('../../images/spaceFace.jpg')} />
                 </div>
 
-                <div className="top-center-home tallBox">
+                <div className="top-center-home">
                     <div className="top-text">
-                        <Typist>
+                    <Typist>
                         <p> Welcome to NASA </p>
                         <Typist.Delay ms={500} />
                         <br />
-                        ALIENS EXIST!
-                        <Typist.Backspace count={13} delay={200} />
+                        ALIENS EXIST!!!
+                        <Typist.Backspace count={15} delay={200} />
                         Ignore that...
                         <br />
                         <Typist.Delay ms={500} />
@@ -50,12 +41,14 @@ export default class Homepage extends Component {
                         </Typist>
                     </div>
                 </div>
+            
 
-                <div className="top-right-home tallBox">
-                    <div className="top-rights-home">
+                <div className="top-right-home">
+                    <div className='top-right-pics'>
                         <div className="sub1-top-right-home">
-                            <img src={require('../../images/planetSun.jpg')} />
+                            <img className="sub1" src={require('../../images/aurora-iss.jpg')} />
                         </div>
+                        
                         <div className="sub2-top-right-home">
                             <img src={require('../../images/hubble.jpg')} />
                         </div>
@@ -66,12 +59,14 @@ export default class Homepage extends Component {
                     </div>
                 </div>
             </div>
+          
 
             <div className="middle-home">
-                <div className="middle-center-home">
+                <div className="middle-left-home">
                     <img src={require('../../images/ISSLady.jpg')} />
                 </div>
-                <div className="middle-left-home">
+
+                <div className="middle-center-home">
                     <img src={require('../../images/voyager.png')} />
                 </div>
 
@@ -98,9 +93,8 @@ export default class Homepage extends Component {
                     <img src={require('../../images/voyagerFloat.jpg')} />
                 </div>
             </div>
-              
           </div>
-          </div>
+        </div>
       )
   }
 }
